f7e817b8d028da139080913511469c77088658b1
hhg000726 <93714318+hhg000726@users.noreply.github.com>
2022-07-05 20:59:53 +0900

    Update 34214-1078-1-two-sum.py

--- 34214-1078-1-two-sum/34214-1078-1-two-sum.py
+++ 34214-1078-1-two-sum/34214-1078-1-two-sum.py
@@ -3,3 +3,4 @@ class Solution:
         for i in range(len(nums)):
             if target - nums[i] in nums[i + 1:]:
                 return [i, nums[i + 1:].index(target - nums[i]) + i + 1]
+    #Check if there is the number that target - each number in leftover list(from i + 1 to end). If it is, return i and that's index.
