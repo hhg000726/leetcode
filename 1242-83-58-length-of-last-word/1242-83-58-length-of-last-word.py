58450244103460e038d8edba2f72f33377c41c62
hhg000726 <93714318+hhg000726@users.noreply.github.com>
2022-07-05 20:52:29 +0900

    Update 1242-83-58-length-of-last-word.py

--- 1242-83-58-length-of-last-word/1242-83-58-length-of-last-word.py
+++ 1242-83-58-length-of-last-word/1242-83-58-length-of-last-word.py
@@ -2,3 +2,4 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         a = s.split()
         return len(a[-1])
+    #split() function split the string by some standards(default is 'space bar')
