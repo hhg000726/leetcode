5e26f4c21f79a5ea2e4c27dc5fc0827781a4a387
hhg000726 <93714318+hhg000726@users.noreply.github.com>
2022-07-06 16:08:20 +0900

    Update 509-fibonacci-number.py

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -5,3 +5,4 @@ class Solution:
         for _ in range(n):
             dp1, dp2 = dp2, dp1 + dp2
         return dp1
+    #Fibonacci Number is one of the most famous and simple DP problem. The main point is that we only need to memory two values to find next value.
