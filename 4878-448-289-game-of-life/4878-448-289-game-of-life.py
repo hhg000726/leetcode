5b2f33407e3e8b4a1ebf88540a2bc06cae16806b
hhg000726 <93714318+hhg000726@users.noreply.github.com>
2022-07-07 19:50:37 +0900

    Update 4878-448-289-game-of-life.py

--- 4878-448-289-game-of-life/4878-448-289-game-of-life.py
+++ 4878-448-289-game-of-life/4878-448-289-game-of-life.py
@@ -16,3 +16,4 @@ class Solution:
                 else:
                     if result[i][j] < 2 or result[i][j] > 3:
                         board[i][j] = 0
+    #전형적인 시뮬레이션 문제. 주변의 값들에 대해 물어보는 문제니 그 값들을 유효한 범위에 저장해주고 조건에 따라 조정만하면 된다.
