86864568bed0162d11e90623e912d9c50cbcfbf4
hhg000726 <93714318+hhg000726@users.noreply.github.com>
2022-07-21 16:36:29 +0900

    Time: 1767 ms (47.52%), Space: 28.2 MB (47.71%) - LeetHub

---
+++ 164-maximum-gap/164-maximum-gap.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        ans = 0
+        nums.sort()
+        for i in range(1, len(nums)):
+            ans = max(ans, nums[i] - nums[i - 1])
+        return ans
